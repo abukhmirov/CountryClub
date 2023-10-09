bd0448539b98ca58a0e46623c8ea6e3183744b60
abukhmirov <alexbukhmirov@gmail.com>
2023-10-09 12:05:04 -0600

    CRUD Created

--- CountryClubAPI/Controllers/MembersController.cs
+++ CountryClubAPI/Controllers/MembersController.cs
@@ -1,6 +1,8 @@
 ﻿using CountryClubAPI.DataAccess;
+using CountryClubAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CountryClubAPI.Controllers
 {
@@ -15,11 +17,85 @@ namespace CountryClubAPI.Controllers
             _context = context;
         }
 
+
+
         public IActionResult AllMembers()
         {
             var members = _context.Members;
 
             return new JsonResult(members);
+        }
+
+
+
+        [Route("/api/members/{id:int}")]
+        public ActionResult SingleMember(int id)
+        {
+            var member = _context.Members.SingleOrDefault(m => m.Id == id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(member);
+
+        }
+
+
+
+        [HttpPost]
+        public ActionResult CreateMember(Member member)
+        {
+            if (member == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Members.Add(member);
+            _context.SaveChanges();
+
+            return new JsonResult(member);
+        }
+
+
+
+
+        [HttpPut]
+        [Route("/api/members/{id:int}")]
+        public IActionResult UpdateMember(int id, Member member)
+        {
+            if (id != member.Id || member == null)
+            {
+                return BadRequest();
+            }
+
+            member.Id = id;
+
+            _context.Members.Update(member);
+            _context.SaveChanges();
+
+            return Redirect($"/api/members/{member.Id}");
+
+        }
+
+
+
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var member = _context.Members.Find(id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            _context.Members.Remove(member);
+            _context.SaveChanges();
+
+            return Content("Deleted");
         }
     }
 }
